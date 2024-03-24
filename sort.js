2b5a4f10d8963a4c0c0fe534f79dae93a0eade2c
Franco da Ribeira <francodaribeira@gmail.com>
2024-03-24 17:55:46 +0200

    add pertinent module file

---
+++ sort.js
@@ -0,0 +1,69 @@
+
+/**
+ * 
+ * @param {[]} arr 
+ * @param {number} i 
+ * @param {number} j 
+ */
+function exchangeValues(arr, i, j) {
+    let iValue = arr[i];
+    arr[i] = arr[j];
+    arr[j] = iValue;
+}
+
+/**
+ * @param {[]} arr
+ * @param {(a, b) => boolean} isAGreaterThanB
+ */
+function selectionSort(arr, isAGreaterThanB) {
+    
+    for (let i = 0; arr.length; i++) {
+        let minIndex = i;
+
+        for (j = i; j < arr.length; j++) {
+            if (isAGreaterThanB(arr[minIndex], arr[j])) minIndex = j;
+        }
+
+        if (minIndex !== i) exchangeValues(arr, i, minIndex);        
+    }
+}
+
+/**
+ * 
+ * @param {[]} arr 
+ * @param {(a, b) => boolean} isAGreaterThanB 
+ */
+function insertionSort(arr, isAGreaterThanB) {
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = i; j > 0 && isAGreaterThanB(arr[j], arr[j - 1]); j--) {
+            exchangeValues(arr, j, j-1);
+        }
+    }
+}
+
+/**
+ * 
+ * @param {[]} arr 
+ * @param {(a, b) => boolean} isAGreaterThanB 
+ */
+function shellSort(arr, isAGreaterThanB) {
+ 
+}
+
+/**
+ * 
+ * @param {[]} arr 
+ * @param {(a, b) => boolean} isAGreaterThanB 
+ */
+function TDMergeSort(arr, isAGreaterThanB) {
+
+}
+
+/**
+ * 
+ * @param {[]} arr 
+ * @param {(a, b) => boolean} isAGreaterThanB 
+ */
+function BUMergeSort(arr, isAGreaterThanB) {
+
+}
